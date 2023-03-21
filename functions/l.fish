a4d3952ced474a5b383729803ff2651fc92ff871
grufino-blackbird <gabriel@blackbird.ai>
2023-03-20 23:00:59 -0300

    initial commit with full setup

---
+++ functions/l.fish
@@ -0,0 +1,4 @@
+function l --wraps='exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --wraps='exa -lah --icons --color=auto' --description 'alias l=exa -lah --icons --color=auto'
+  exa -lah --icons --color=auto $argv
+        
+end
---
+++ functions/lt.fish
@@ -0,0 +1,4 @@
+function lt --wraps='exa --icons --color=auto --tree --level=2' --description 'alias lt=exa --icons --color=auto --tree --level=2'
+  exa --icons --color=auto --tree --level=2 $argv
+        
+end
